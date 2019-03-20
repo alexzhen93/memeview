e75486e939a26f36676be950159f50636378a8c2
Darius <sanagusd@bxscience.edu>
2019-03-20 18:20:20 -0400

    posts can now show posts

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Posts
+
 # Create your views here.
 def index(request):
-	return render(request, 'posts/index.html')
+
+	# First 10 post objects
+	posts = Posts.objects.all()[:10]
+
+
+
+	context = {
+		'title': "Post Feed",
+		'posts': posts
+	}
+
+	return render(request, 'posts/index.html', context)
+
+
+def details(request, id):
+	post = Posts.objects.get(id=id)
+
+	context = {
+		'post': post
+	}
+
+	return render(request, 'posts/details.html', context)
--- seeddb/admin.py
+++ seeddb/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models imports Posts
-
-admin.site.register(Posts)
