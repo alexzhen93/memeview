0c21bb551e339751225ed6662f7742391833e21d
Darius <darius.sanagustin116@gmail.com>
2019-03-28 21:16:06 -0400

    made a fire ass landing page

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+	context = {
+		'css': 'landing/landing.css'
+	}
+	return render(request, 'landing/index.html', context)
