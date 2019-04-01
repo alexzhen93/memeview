9f5dc91ada368b33afb87790fb4a1025ec04799c
Darius <sanagusd@bxscience.edu>
2019-04-01 18:05:46 -0400

    fixed migrations

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from datetime import datetime
+from django.conf import settings
 
 # Create your models here.
 class Posts(models.Model):
 	caption = models.CharField(max_length=255)
 	image = models.CharField(max_length=255)
 	created_at = models.DateTimeField(default=datetime.now, blank=True)
-	author = models.ForeignKey(auth_user, on_delete=models.CASCADE)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.caption[0:10] + "..."
