257a40c89128250a46b119f5e909ffac7bcb0faf
Darius San Agustin <darius.sanagustin116@gmail.com>
2019-04-08 12:53:20 -0400

    Revert "started creating seed database sql script"
    This reverts commit 18fd49a8d9eb04e3caa59ff5796932cca51f04db.
    
    "reverts branch-off"

--- memes.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO posts_posts (id, caption, image, created_at) VALUES(
-	3, 
-	'When the enemy team folds after hearing you shout "Allfather give me sight!"', 
-	"https://cdn.discordapp.com/attachments/387403401916317697/556953352043495465/why-running-flare-fried.png", 
-	"2019-03-22 14:46:40.000000"
-);
