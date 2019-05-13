5b4872ad4103b512ec0b767a3971851da32d981c
Darius San Agustin <darius.sanagustin116@gmail.com>
2019-05-13 17:09:36 -0400

    fixed settings.py for database permissions

--- memeview/settings.py
+++ memeview/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'memeviewdb',
-        'USER': 'root',
-        'PASSWORD': 'password',
+        'USER': 'lgd116',
+        'PASSWORD': 'bungabros',
         'HOST': 'localhost',
         'PORT': ''
     }
